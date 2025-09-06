620f71acbc3567bc1d122418d2a82e57a53f17c3
fabianosan85 <162376261+fabianosan85@users.noreply.github.com>
2025-09-06 10:38:05 -0300

    Update jurossimples.js

--- jurossimples.js
+++ jurossimples.js
@@ -1,16 +1,16 @@
 let capital=1000; // em R$
 let taxa=5; // em %
-let tempo =12 // em meses
+let tempoMeses=12 // em meses
 let montante = capital * (1 + taxa * tempoMeses);
 console.log("capital:" + "R$" + capital + " ");
 console.log("taxa:" + taxa * 100 + "%");
-console.log("tempo:" + tempo + "meses");
+console.log("tempoMeses:" + tempoMeses + "meses");
 console.log("O montante é?:" + montante.toFixed(2));
 
 function calcularjuros(){
     let capital = parseFloat(document.getElementById("capital").value);
     let taxa = parseFloat(document.getElementById("taxa").value) / 100;
-    let tempo = parseFloat(document.getElementById("tempoMeses").value);
+    let tempoMeses = parseFloat(document.getElementById("tempoMeses").value);
     let montante = capital * (1 + taxa * tempoMeses);
     document.getElementById("resultado").value = "R$" + montante.toFixed(2);
 }
